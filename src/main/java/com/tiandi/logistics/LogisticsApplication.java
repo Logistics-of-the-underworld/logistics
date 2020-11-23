b4407a0c7d27fd75e246c3bd9bed5ed5540af973
YueWu <17184030601@163.com>
2020-11-23 15:29:03 +0800

    init

---
+++ src/main/java/com/tiandi/logistics/LogisticsApplication.java
@@ -0,0 +1,18 @@
+package com.tiandi.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yue Wu
+ * @version 1.0
+ * @since 2020/11/23 15:25
+ */
+@SpringBootApplication
+public class LogisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsApplication.class, args);
+    }
+
+}
