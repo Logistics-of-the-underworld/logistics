4ea26c33fd51f8fb02b6921b3a919363c33d4636
wonichan <1012403910@qq.com>
2020-12-04 11:08:07 +0800

    修改配送站点

--- src/main/java/com/tiandi/logistics/controller/DistributionController.java
+++ src/main/java/com/tiandi/logistics/controller/DistributionController.java
@@ -13,7 +13,10 @@ import com.tiandi.logistics.aop.log.enumeration.SysTypeEnum;
 import com.tiandi.logistics.entity.pojo.Distribution;
 import com.tiandi.logistics.entity.result.ResultMap;
 import com.tiandi.logistics.service.DistributionService;
+import com.tiandi.logistics.utils.JWTUtil;
 import io.swagger.annotations.*;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,50 +41,40 @@ public class DistributionController {
      * 根据身份及权限查询配送点所有信息
      * @return
      */
-    @PostMapping("/getAllDistribution")
-    @ControllerLogAnnotation(remark = "查询配送点所有信息",sysType = SysTypeEnum.ADMIN,opType = OpTypeEnum.SELECT)
+    @GetMapping("/getAllDistribution/{{name_company}}")
     @ApiOperation(value = "获取配送点信息接口",notes = "当前配送点信息包括配送点的编码和名称、管理员代码和姓名、管理员\t口令、确认口令以及上行站点和下行站点等")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "name_company", value = "所属公司", paramType = "query", dataType = "String"),
-            @ApiImplicitParam(name = "role",value = "身份",required = true,paramType = "query",dataType = "String"),
-            @ApiImplicitParam(name = "permission",value = "权限",required = true,paramType = "query",dataType = "String"),
-            @ApiImplicitParam(name = "id_distribution",value = "配送点编码",paramType = "query",dataType = "String")
     })
     @ApiResponses({
             @ApiResponse(code = 40000, message = "配送点信息查询成功！"),
             @ApiResponse(code = 50011, message = "配送点信息查询失败，请重试！")
     })
-    public ResultMap getAllDistribution(@RequestParam(value = "name_company",required = false) String name_company, @RequestParam("role") String role, @RequestParam String permission, @RequestParam(value = "id_distribution",required = false) String id_distribution){
-        if (permission.equals("root") && role.equals("admin")){
-            List<Distribution> distributionList1 = distributionService.getBaseMapper().selectList(new QueryWrapper<Distribution>());
-            for (Distribution x:distributionList1
-                 ) {
-                System.out.println(x);
-            }
-            String distributionList  = JSON.toJSONString(distributionList1);
-            resultMap.success().message("配送点信息查询成功！").addElement("distributionList",distributionList);
-        }else if (permission.equals("admin") && role.equals("distribution") && name_company != null){
-            List<Distribution> distributionList1 = distributionService.getBaseMapper().selectList(new QueryWrapper<Distribution>().eq("name_company", name_company));
-            for (Distribution x:distributionList1
-            ) {
-                System.out.println(x);
-            }
-            String distributionList  = JSON.toJSONString(distributionList1);
-            resultMap.success().message("配送点信息查询成功！").addElement("distributionList",distributionList);
-        }else if (permission.equals("admin") && role.equals("distribution") && id_distribution != null){
-            List<Distribution> distributionList1 = distributionService.getBaseMapper().selectList(new QueryWrapper<Distribution>().eq("id_distribution", id_distribution));
-            for (Distribution x:distributionList1
-            ) {
-                System.out.println(x);
-            }
-            String distributionList  = JSON.toJSONString(distributionList1);
-            resultMap.success().message("配送点信息查询成功！").addElement("distributionList",distributionList);
-        }else if (name_company == null && id_distribution == null){
-            resultMap.fail().code(50011).message("密码错误或账户不存在!");
-        }else {
-            resultMap.fail().code(50011).message("密码错误或账户不存在!");
+    public ResultMap getAllDistribution(@RequestHeader String token,@PathVariable(value = "name_company",required = false) String name_company){
+        String permission = JWTUtil.getUserPermission(token);
+        String role = JWTUtil.getUserRole(token);
+        if ("root".equals(permission) && "admin".equals(role)){
+            List<Distribution> distributionList = distributionService.list();
+            resultMap.success().message("配送点信息查询成功！").addElement("data",distributionList);
+            return resultMap;
+        } else if ("admin".equals(permission) && "distribution".equals(role) && name_company != null){
+            List<Distribution> distributionList = distributionService.getBaseMapper().selectList(new QueryWrapper<Distribution>().eq("name_company", name_company));
+            resultMap.success().message("配送点信息查询成功！").addElement("data",distributionList);
+            return resultMap;
         }
-        return resultMap;
+        return resultMap.fail();
+    }
+
+    @GetMapping("/getDistributionByID/{id_distribution}")
+        public ResultMap GetDistributionByID(@RequestHeader String token,@PathVariable("id_distribution") String id_distribution){
+        String permission = JWTUtil.getUserPermission(token);
+        String role = JWTUtil.getUserRole(token);
+        if ("admin".equals(permission) && "distribution".equals(role) && id_distribution != null){
+            List<Distribution> distributionList = distributionService.getBaseMapper().selectList(new QueryWrapper<Distribution>().eq("id_distribution", id_distribution));
+            resultMap.success().message("配送点信息查询成功！").addElement("data",distributionList);
+            return resultMap;
+        }
+        return resultMap.fail();
     }
 
     @PostMapping("/updateDistribution")
@@ -96,14 +89,10 @@ public class DistributionController {
             @ApiResponse(code = 50011, message = "配送点更新失败，请重试！")
     })
     public ResultMap updateDistribution(@RequestParam("id_distribution") String id_distribution,@RequestParam("distribution") String distribution){
-
-        System.out.println(distribution.toString());
         if (distribution == null || "".equals(distribution)){
             resultMap.fail().code(40010).message("服务器内部错误!");
         }
         Distribution distribution1 = JSON.parseObject(distribution,Distribution.class);
-        distribution1.setIdDistribution(Integer.valueOf(id_distribution));
-        System.out.println(distribution1.toString());
         int updatedistribution = distributionService.updatedistribution(distribution1);
         if (updatedistribution == 1){
             resultMap.success().message("更新成功!");
